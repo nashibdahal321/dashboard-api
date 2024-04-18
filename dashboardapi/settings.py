7b647e6d44636e736cc24dbb21f4f884c1e4607f
Nashib Dahal <nashibdahal@gmail.com>
2024-04-17 22:06:30 -0700

    add localhost as allowed origins

--- dashboardapi/settings.py
+++ dashboardapi/settings.py
@@ -122,4 +122,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",  
+    ]
+
